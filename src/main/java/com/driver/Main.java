0590fae0639ad06c533e3cf036f856a50e8ac5b3
Pushpak <pushpakmali44@gmail.com>
2023-05-16 19:17:57 +0530

    Changes Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String args[]){
-        A obj = new B();
+        B obj = new B();
         String ans = obj.meth();
-    }
-}
-class A{
-    public static String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A{
-    public static String meth(){
-        return "Method is overridden in Extended class B";
     }
 }
