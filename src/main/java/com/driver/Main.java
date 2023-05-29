2ee8d2a7fc2f1b80f48dbeec2521122188aa0ee4
Pushpak <pushpakmali44@gmail.com>
2023-05-29 22:38:08 +0530

    Trying with static classes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,28 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         B object = new B();
         object.meth();
         object.meth();
     }
-}
 
-class A{
-    public A(){}
+    static class A {
+        public A() {
+        }
 
-    public static String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A{
-    public B() {}
-
-    public static String meth(){
-        return "Method is overridden in Extendend class B";
+        public static String meth() {
+            return "Invoking method from class A";
+        }
     }
 
+    static class B extends A {
+        public B() {
+        }
+
+        public static String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
 }
